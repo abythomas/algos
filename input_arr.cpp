b7daf173dbdc935cd662e6272793c6be4a9efb3d
Aby Thomas <abythomas2897@gmail.com>
2018-07-24 16:39:24 +0530

    Create input_arr.cpp

---
+++ input_arr.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+  cout<<"Enter number of test cases: ";
+  int t,n,i,j;
+  cin>>t;
+  char c=getchar();
+  for(i=0;i<t;i++){
+    string str;
+    getline(cin,str);
+    const char *cstr=str.c_str();
+    char *token = strtok((char *)cstr, " ");
+    vector <int> arr;
+    int temp;
+    j=0;
+    while(token!=NULL){
+      arr.push_back(atoi(token));
+      cout<<arr[j]<<" ";
+      token=strtok(NULL," ");
+      j++;
+    }
+  }
+}
